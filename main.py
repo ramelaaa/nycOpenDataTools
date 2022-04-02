d92f1e4ad7024bf8f8183ebebf6c3d2e26b9686d
ramelaaa <ramelaramnauth2@gmail.com>
2022-04-02 14:11:20 -0400

    oath.py done

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from plates import plates 
 from oathClass import oath
 
-oath1 = oath("RAMNAUTH", "RAMELA")
+oath1 = oath("RAMNAUTH", "SURUJNAUTH")
 oath2 = oath("BRONX","03997","0068")
 
 
 
+oath1.getInfo()
+oath2.getInfo()
 
+oath1.createDataFrame()
+oath2.createDataFrame()
+
+oath1.print()
+oath2.print()
 
 
 # plate = plates("HAC7330", "NY")
--- oath.py
+++ oath.py
@@ -1,66 +1,31 @@
 #!/usr/bin/env python
-
 import pandas as pd
 from sodapy import Socrata
 
-pd.options.display.max_columns = None
-pd.set_option('display.max_colwidth', None)
-client = Socrata("data.cityofnewyork.us", None)
+class oath:
+    def __init__(self, *args):
+        if(len(args) == 2):
+            self.lastname = args[0]
+            self.firstname = args[1]
+            self.type = "name"
+        elif(len(args) == 3):
+            self.borough = args[0]
+            self.block = args[1]
+            self.lot = args[2]  
+            self.type = "bbl" 
+        else:
+            "None" 
+        self.client = Socrata("data.cityofnewyork.us", None)
 
-dataset_id = "jz4z-kudi"
+    def getInfo(self):
+        if(self.type == "name"):
+            self.data = self.client.get("jz4z-kudi",where="respondent_last_name='" + self.lastname + "' AND respondent_first_name ='" + self.firstname + "'",limit=100000)
 
-lastname    = "respondent_last_name='RAMNAUTH'"
-surujnauth  = "respondent_first_name='SURUJNAUTH'"
-toumwatee   = "respondent_first_name='TOUMWATEE'"
-ramela      = "respondent_first_name='RAMELA'"
-sANDt       = "respondent_first_name='SURUJNAUTHTOUMWAT'"
+        if(self.type == "bbl"):
+            self.data = self.client.get("jz4z-kudi",where="violation_location_borough='" + self.borough +"' AND violation_location_block_no='" +self.block + "' AND violation_location_lot_no = '" +self.lot +"'",limit=100000)
 
-borough = "violation_location_borough='BRONX'"
-block   = "violation_location_block_no='03997'"
-lot     = "violation_location_lot_no='0068'"
+    def createDataFrame(self):
+        self.violations_df = pd.DataFrame.from_records(self.data)
 
-responseLimit = 20
-
-surujnauthQuery          = client.get(dataset_id,where=lastname + " AND " + surujnauth ,limit=responseLimit)
-toumwateeQuery           = client.get(dataset_id,where=lastname + " AND " + toumwatee,limit=responseLimit)
-ramelaQuery              = client.get(dataset_id,where=lastname + " AND " + ramela ,limit=responseLimit)
-surujnauthtoumwatQuery   = client.get(dataset_id,where=sANDt,limit=responseLimit)
-homeQuery                = client.get(dataset_id,where= borough + " AND " + block + " AND " + lot,limit = responseLimit)
-
-# Create DataFrames for each data set
-toumwatee_df            = pd.DataFrame.from_records(toumwateeQuery)
-ramela_df               = pd.DataFrame.from_records(ramelaQuery)
-surujnauth_df           = pd.DataFrame.from_records(surujnauthQuery)
-surujnauthtoumwat_df    = pd.DataFrame.from_records(surujnauthtoumwatQuery)
-home_df                 = pd.DataFrame.from_records(homeQuery)
-
-# print rresuls 
-print("- Surujnauth Ramnauth: ")
-if surujnauth_df.empty:
-    print ("No Tickets")
-else:
-    print("Ticket Number: " + surujnauth_df["ticket_number"] + "  Paid:$" + surujnauth_df["paid_amount"] + " " + surujnauth_df["issuing_agency"])
-
-print("- Toumwatee Ramnauth: ")
-if toumwatee_df.empty:
-    print ("No Tickets")
-else:
-    print("Ticket Number: " + toumwatee_df["ticket_number"] + " Paid:$" + toumwatee_df["paid_amount"]+ " " + toumwatee_df["issuing_agency"])
-
-print("- Surujnauth and Toumwatee Ramnauth: ")
-if surujnauthtoumwat_df.empty:
-    print ("No Tickets")
-else:
-    print("Ticket Number: " + surujnauthtoumwat_df["ticket_number"] + "  Paid:$" + surujnauthtoumwat_df["paid_amount"]+ " " + surujnauthtoumwat_df["issuing_agency"])
-
-print("- Ramela Ramnauth: ")
-if ramela_df.empty:
-    print ("No Tickets")
-else:
-    print("Ticket Number: " + ramela_df["ticket_number"] + " Paid:$" + ramela_df["paid_amount"]+ " " + ramela_df["issuing_agency"])
-
-print("- Home")
-if home_df.empty:
-    print ("No Tickets")
-else:
-    print("Ticket Number: " + home_df["ticket_number"] + " Paid:$" + home_df["paid_amount"] + " " + home_df["issuing_agency"] + " " + home_df["violation_date"] +" " +home_df["respondent_last_name"])
+    def print(self):
+        print(self.violations_df[["ticket_number","issuing_agency","violation_date","penalty_imposed","paid_amount","respondent_last_name"]])
