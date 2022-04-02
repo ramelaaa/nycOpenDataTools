085a6ce7240c0f1eb3467fee285424b5216793dd
ramelaaa <ramelaramnauth2@gmail.com>
2022-04-02 09:56:12 -0400

    clean data fram works

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from platesClass import plates
 plate = plates("HAC7330", "NY")
 
 plate.getInfo()
+plate.createDataFrame()
+plate.cleanDataFrame()
--- platesClass.py
+++ platesClass.py
@@ -10,3 +10,10 @@ class plates:
 
     def getInfo(self):
         self.data = self.client.get("nc67-uf89",where="plate='" + self.plateNumber + "'AND state ='" + self.state + "'",limit=100000)
+
+    def createDataFrame(self):
+        self.tickets_df = pd.DataFrame.from_records(self.data)
+    
+    def cleanDataFrame(self):
+        self.tickets_df["amount_due"] = pd.to_numeric(self.tickets_df["amount_due"])
+        self.tickets_df["issue_date"] = pd.to_datetime(self.tickets_df["issue_date"])
