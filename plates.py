0032e10a2bffbf484c163651d0939d89ded3fbb4
ramelaaa <ramelaramnauth2@gmail.com>
2022-04-02 10:15:03 -0400

    moved platedclass to plates

--- plates.py
+++ plates.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python
 import pandas as pd
 from sodapy import Socrata
-import sys
 
-client = Socrata("data.cityofnewyork.us", None)
-if(len(sys.argv) != 3):
-    print("Expected three arguments")
-    print("python3 plates.py [plate] [state]")
-    exit()
+class plates:
+    def __init__(self, plateNumber, state):
+        self.plateNumber = plateNumber
+        self.state = state
+        self.client = client = Socrata("data.cityofnewyork.us", None)
 
-whereClause = "plate='" + sys.argv[1] + "' AND state = '" + sys.argv[2] + "'"
-tickets= client.get("nc67-uf89",where=whereClause,limit=100000)
-tickets_df = pd.DataFrame.from_records(tickets)
-ticket = "amount_due"
-tickets_df[ticket] = pd.to_numeric(tickets_df[ticket])
-tickets_df["issue_date"] = pd.to_datetime(tickets_df["issue_date"])
-print("Plate: ",sys.argv[1])
-print("Amount Due: $",tickets_df[ticket].sum())
-tickets_df = tickets_df.sort_values(by="issue_date")
-print(tickets_df[["summons_number", "issue_date", ticket, "violation"]])
+    def getInfo(self):
+        self.data = self.client.get("nc67-uf89",where="plate='" + self.plateNumber + "'AND state ='" + self.state + "'",limit=100000)
+
+    def createDataFrame(self):
+        self.tickets_df = pd.DataFrame.from_records(self.data)
+    
+    def cleanDataFrame(self):
+        self.tickets_df["amount_due"] = pd.to_numeric(self.tickets_df["amount_due"])
+        self.tickets_df["issue_date"] = pd.to_datetime(self.tickets_df["issue_date"])
+        self.tickets_df = self.tickets_df.sort_values(by="issue_date")
+
+    def print(self):
+        print(self.plateNumber, self.state)
+        print("Amount Due: $",self.tickets_df["amount_due"].sum())
+        print(self.tickets_df[["summons_number", "issue_date", "amount_due", "violation"]])
